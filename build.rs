d2c0a9c3ef2a454a8ced644dfa35e37f29212685
Romain Chardiny <romain.chardiny@gmail.com>
2025-03-24 12:24:45 +0100

    generate man pages

--- build.rs
+++ build.rs
@@ -6,14 +6,26 @@ fn main() {
     use clap::{CommandFactory, ValueEnum};
     use clap_complete::{generate_to, Shell};
 
-    let out_dir = env::var("SHELL_COMPLETIONS_DIR")
+    let shell_comp_out_dir = env::var("SHELL_COMPLETIONS_DIR")
         .or_else(|_| env::var("OUT_DIR"))
         .unwrap();
 
-    fs::create_dir_all(&out_dir).unwrap();
+    let man_pages_out_dir = env::var("MAN_PAGES_DIR")
+        .or_else(|_| env::var("OUT_DIR"))
+        .unwrap();
+
+    fs::create_dir_all(&shell_comp_out_dir).unwrap();
 
     let mut cmd = Args::command();
     for &shell in Shell::value_variants() {
-        generate_to(shell, &mut cmd, "floc", &out_dir).unwrap();
+        generate_to(shell, &mut cmd, "floc", &shell_comp_out_dir).unwrap();
     }
+
+    fs::create_dir_all(&man_pages_out_dir).unwrap();
+
+    let man = clap_mangen::Man::new(cmd);
+    let mut buf = Vec::new();
+    man.render(&mut buf).unwrap();
+
+    std::fs::write(PathBuf::from(&man_pages_out_dir).join("floc.1"), buf).unwrap();
 }
