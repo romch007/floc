c0ba43d357dabd64fb305a62ed7cc87fd79f9ddb
Romain Chardiny <romain.chardiny@gmail.com>
2025-11-18 11:08:08 +0100

    format c++ files

--- llvm-wrapper/wrapper.cpp
+++ llvm-wrapper/wrapper.cpp
@@ -1,21 +1,23 @@
 #include "wrapper.h"
 
-#include <llvm/TargetParser/Triple.h>
-#include <llvm/IR/Module.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/InlineAsm.h>
+#include <llvm/IR/Module.h>
+#include <llvm/TargetParser/Triple.h>
 
 extern "C" {
 
-arch_t arch_from_target_triple(const char* target_triple) {
+arch_t arch_from_target_triple(const char *target_triple) {
   llvm::Triple triple(target_triple);
 
   switch (triple.getArch()) {
-  #define X(name) case llvm::Triple::name: return arch_##name;
+#define X(name)                                                                \
+  case llvm::Triple::name:                                                     \
+    return arch_##name;
     ARCH_LIST
-  #undef X
-    default:
-      return arch_unknown;
+#undef X
+  default:
+    return arch_unknown;
   }
 }
 
@@ -25,25 +27,26 @@ int is_msvc(const char *target_triple) {
   return triple.getEnvironment() == llvm::Triple::EnvironmentType::MSVC;
 }
 
-void add_comment_section(LLVMModuleRef module_ref, const char* compiler_string) {
-  llvm::Module* module = llvm::unwrap(module_ref);
-  llvm::LLVMContext& context = module->getContext();
+void add_comment_section(LLVMModuleRef module_ref,
+                         const char *compiler_string) {
+  llvm::Module *module = llvm::unwrap(module_ref);
+  llvm::LLVMContext &context = module->getContext();
 
-  std::string asmText =
-      std::string(".section .comment\n") +
-      ".string \"" + compiler_string + "\"\n";
+  std::string asmText = std::string(".section .comment\n") + ".string \"" +
+                        compiler_string + "\"\n";
 
-  llvm::FunctionType* fnTy = llvm::FunctionType::get(llvm::Type::getVoidTy(context), false);
+  llvm::FunctionType *fnTy =
+      llvm::FunctionType::get(llvm::Type::getVoidTy(context), false);
 
-  llvm::Function* fn = llvm::Function::Create(fnTy, llvm::GlobalValue::InternalLinkage, "__emit_comment", module);
+  llvm::Function *fn = llvm::Function::Create(
+      fnTy, llvm::GlobalValue::InternalLinkage, "__emit_comment", module);
 
   llvm::BasicBlock *bb = llvm::BasicBlock::Create(context, "entry", fn);
   llvm::IRBuilder<> builder(bb);
 
-  llvm::InlineAsm* ia = llvm::InlineAsm::get(fnTy, asmText, "", true);
+  llvm::InlineAsm *ia = llvm::InlineAsm::get(fnTy, asmText, "", true);
 
   builder.CreateCall(ia);
   builder.CreateRetVoid();
 }
-
 }
--- llvm-wrapper/wrapper.h
+++ llvm-wrapper/wrapper.h
@@ -7,79 +7,79 @@ extern "C" {
 
 #include <llvm-c/Core.h>
 
-#define ARCH_LIST \
-  X(arm) \
-  X(armeb) \
-  X(aarch64) \
-  X(aarch64_be) \
-  X(aarch64_32) \
-  X(arc) \
-  X(avr) \
-  X(bpfel) \
-  X(bpfeb) \
-  X(csky) \
-  X(dxil) \
-  X(hexagon) \
-  X(loongarch32) \
-  X(loongarch64) \
-  X(m68k) \
-  X(mips) \
-  X(mipsel) \
-  X(mips64) \
-  X(mips64el) \
-  X(msp430) \
-  X(ppc) \
-  X(ppcle) \
-  X(ppc64) \
-  X(ppc64le) \
-  X(r600) \
-  X(amdgcn) \
-  X(riscv32) \
-  X(riscv64) \
-  X(sparc) \
-  X(sparcv9) \
-  X(sparcel) \
-  X(systemz) \
-  X(tce) \
-  X(tcele) \
-  X(thumb) \
-  X(thumbeb) \
-  X(x86) \
-  X(x86_64) \
-  X(xcore) \
-  X(xtensa) \
-  X(nvptx) \
-  X(nvptx64) \
-  X(amdil) \
-  X(amdil64) \
-  X(hsail) \
-  X(hsail64) \
-  X(spir) \
-  X(spir64) \
-  X(spirv) \
-  X(spirv32) \
-  X(spirv64) \
-  X(kalimba) \
-  X(shave) \
-  X(lanai) \
-  X(wasm32) \
-  X(wasm64) \
-  X(renderscript32) \
-  X(renderscript64) \
+#define ARCH_LIST                                                              \
+  X(arm)                                                                       \
+  X(armeb)                                                                     \
+  X(aarch64)                                                                   \
+  X(aarch64_be)                                                                \
+  X(aarch64_32)                                                                \
+  X(arc)                                                                       \
+  X(avr)                                                                       \
+  X(bpfel)                                                                     \
+  X(bpfeb)                                                                     \
+  X(csky)                                                                      \
+  X(dxil)                                                                      \
+  X(hexagon)                                                                   \
+  X(loongarch32)                                                               \
+  X(loongarch64)                                                               \
+  X(m68k)                                                                      \
+  X(mips)                                                                      \
+  X(mipsel)                                                                    \
+  X(mips64)                                                                    \
+  X(mips64el)                                                                  \
+  X(msp430)                                                                    \
+  X(ppc)                                                                       \
+  X(ppcle)                                                                     \
+  X(ppc64)                                                                     \
+  X(ppc64le)                                                                   \
+  X(r600)                                                                      \
+  X(amdgcn)                                                                    \
+  X(riscv32)                                                                   \
+  X(riscv64)                                                                   \
+  X(sparc)                                                                     \
+  X(sparcv9)                                                                   \
+  X(sparcel)                                                                   \
+  X(systemz)                                                                   \
+  X(tce)                                                                       \
+  X(tcele)                                                                     \
+  X(thumb)                                                                     \
+  X(thumbeb)                                                                   \
+  X(x86)                                                                       \
+  X(x86_64)                                                                    \
+  X(xcore)                                                                     \
+  X(xtensa)                                                                    \
+  X(nvptx)                                                                     \
+  X(nvptx64)                                                                   \
+  X(amdil)                                                                     \
+  X(amdil64)                                                                   \
+  X(hsail)                                                                     \
+  X(hsail64)                                                                   \
+  X(spir)                                                                      \
+  X(spir64)                                                                    \
+  X(spirv)                                                                     \
+  X(spirv32)                                                                   \
+  X(spirv64)                                                                   \
+  X(kalimba)                                                                   \
+  X(shave)                                                                     \
+  X(lanai)                                                                     \
+  X(wasm32)                                                                    \
+  X(wasm64)                                                                    \
+  X(renderscript32)                                                            \
+  X(renderscript64)                                                            \
   X(ve)
 
 typedef enum {
 #define X(name) arch_##name,
   ARCH_LIST
 #undef X
-  arch_unknown
+      arch_unknown
 } arch_t;
 
 arch_t arch_from_target_triple(const char *target_triple);
 
-int is_msvc(const char* target_triple);
+int is_msvc(const char *target_triple);
 
-void add_comment_section(LLVMModuleRef module_ref, const char* compiler_string);
+void add_comment_section(LLVMModuleRef module_ref, const char *compiler_string);
 
 #ifdef __cplusplus
 }
